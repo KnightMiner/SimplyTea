8fd359f5464ad943c33269b6e84be254dc565b60
Sunconure11 <eyespot7b@aol.com>
2017-07-28 12:37:07 -0400

    Revert "A minor tweak"
    This reverts commit 4c75255b7ed40f43b09cdc969c050d5e503dea82.

--- src/main/java/elucent/simplytea/SimplyTea.java
+++ src/main/java/elucent/simplytea/SimplyTea.java
@@ -53,7 +53,7 @@ import java.util.Random;
 public class SimplyTea {
 	public static final String MODID = "simplytea";
 	public static final String NAME = "Simply Tea!";
-	public static final String VERSION = "1.5";
+	public static final String VERSION = "1.4";
 	public static Random random = new Random();
 
 	@SidedProxy(clientSide = "elucent.simplytea.SimplyTea$ClientProxy", serverSide = "elucent.simplytea.SimplyTea$CommonProxy")
