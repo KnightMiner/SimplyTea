35c641298dc3d650a51fbffc8a92ff7698570aaf
KnightMiner <knightminer4@gmail.com>
2018-12-21 21:06:10 -0600

    Only set the version in one place

--- src/main/java/elucent/simplytea/SimplyTea.java
+++ src/main/java/elucent/simplytea/SimplyTea.java
@@ -31,7 +31,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class SimplyTea {
 	public static final String MODID = "simplytea";
 	public static final String NAME = "Simply Tea!";
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "${version}";
 
 	public static CreativeTabs tab = new CreativeTabs("simplytea") {
 		@Override
