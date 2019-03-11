95f67d911770cda7b2f1115bec58a610afedb427
KnightMiner <knightminer4@gmail.com>
2019-03-10 22:26:46 -0500

    Make caffeinated slightly slower

--- src/main/java/elucent/simplytea/potion/PotionCaffeinated.java
+++ src/main/java/elucent/simplytea/potion/PotionCaffeinated.java
@@ -11,7 +11,7 @@ public class PotionCaffeinated extends ModPotion {
 	public PotionCaffeinated(String name) {
 		super(name, false, 0x66300E);
 
-		this.registerPotionAttributeModifier(SharedMonsterAttributes.MOVEMENT_SPEED, "7BB42B36-75AC-448D-9BE2-B318E42D6898", 0.07, 2);
+		this.registerPotionAttributeModifier(SharedMonsterAttributes.MOVEMENT_SPEED, "7BB42B36-75AC-448D-9BE2-B318E42D6898", 0.06, 2);
 		this.registerPotionAttributeModifier(SharedMonsterAttributes.ATTACK_SPEED, "C3B3B80D-B94D-4C67-83FC-5A893EB98230", 0.05, 2);
 	}
 
