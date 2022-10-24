21653965fe05f5c1f6eb49a476b19db135a62bf4
KnightMiner <knightminer4@gmail.com>
2022-10-24 18:16:11 -0400

    Remove only in usages
    API is deprecated, and not needed where it was being used

--- src/main/java/knightminer/simplytea/item/TooltipItem.java
+++ src/main/java/knightminer/simplytea/item/TooltipItem.java
@@ -1,20 +1,16 @@
 package knightminer.simplytea.item;
 
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.network.chat.Component;
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.util.Lazy;
 
 import javax.annotation.Nullable;
 import java.util.List;
-
-import net.minecraft.world.item.Item.Properties;
 
 public class TooltipItem extends Item {
     private final Lazy<Component> tooltipLine;
@@ -24,7 +20,6 @@ public class TooltipItem extends Item {
     }
 
     @Override
-    @OnlyIn(Dist.CLIENT)
     public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
         tooltip.add(tooltipLine.get());
     }
